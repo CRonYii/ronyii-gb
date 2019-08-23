13ecd5afe3a2331389f7656a2b5b938c5a8230d7
Rongyi Chen <kenchenrong@gmail.com>
2019-08-22 23:36:35 -0700

    put debug options into json, expose emulator to window

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,13 @@
 import Emulator from "./Emulator";
 import { initPage } from "./Page";
 
-export const debugEnabled = {
-    printMemory: false,
-    breakpoints: true,
-    interrupts: true,
-    pauseEveryTick: 4000
-};
+export const debugEnabled: any = require("./debug_options.json");
 
 const { display } = initPage();
 
 export const emulator = new Emulator({
     display
 });
+
+let wnd: any = window;
+wnd.emulator = emulator;
