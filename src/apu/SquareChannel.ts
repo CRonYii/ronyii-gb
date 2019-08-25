2abb3594621195164634319f8884549c03a93729
Rongyi Chen <kenchenrong@gmail.com>
2019-08-25 13:40:56 -0700

    rename wave pattern register

--- src/apu/SquareChannel.ts
+++ src/apu/SquareChannel.ts
@@ -10,7 +10,7 @@ export default class SquareChannel extends SoundUnit {
     public readonly lengthCounter: LengthCounter = new LengthCounter(this, 1 << 6);
 
     private readonly sweepRegister: Register8 = new Register8(); // 0xff10 - NR10
-    private readonly soundLengthWavePattern: Register8 = new Register8(); // 0xff11 - NR11 / 0xff16 - NR21
+    private readonly wavePattern: Register8 = new Register8(); // 0xff11 - NR11 / 0xff16 - NR21
     private readonly volumeEnvelope: Register8 = new Register8(); // 0xff12 - NR12 / 0xff17 - NR22
     private readonly frequencyLow: Register8 = new Register8(); // 0xff13 - NR13 / 0xff18 - NR23
     private readonly frequencyHigh: Register8 = new Register8(); // 0xff14 - NR14 / 0xff19 - NR24
@@ -25,7 +25,7 @@ export default class SquareChannel extends SoundUnit {
         switch (address) {
             case 0x0: return this.sweepRegister.set(data);
             case 0x1:
-                this.soundLengthWavePattern.set(data);
+                this.wavePattern.set(data & 0xc0);
                 this.lengthCounter.reload(data & 0x3f);
                 return;
             case 0x2: return this.volumeEnvelope.set(data);
@@ -40,7 +40,7 @@ export default class SquareChannel extends SoundUnit {
     getByte(address: number): number {
         switch (address) {
             case 0x0: return this.useSweep ? this.sweepRegister.get() | 0x80 : 0xff;
-            case 0x1: return this.soundLengthWavePattern.get() | 0x3f;
+            case 0x1: return this.wavePattern.get() | 0x3f;
             case 0x2: return this.volumeEnvelope.get();
             case 0x3: return 0xff;
             case 0x4: return (this.isLengthCounterEnable() ? 0x40 : 0) | 0xbf;
